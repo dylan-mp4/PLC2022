5a89ed6d752944e36a27ad8093a53ebb80fcc9e0
Dylan <72610316+dylan-mp4@users.noreply.github.com>
2022-01-31 18:45:35 +0000

    task 1.2c

--- lab1-intro/lisp/tasks.lisp
+++ lab1-intro/lisp/tasks.lisp
@@ -15,6 +15,8 @@
 (if (and (> x 9) (< x 50)) (format t "number below 50"))
 (if (> x 49) (format t "number greater or equal 50"))
 ;; code for Task 1.2(c):
-(setf prg '(+ 1 n)) ; define a very simple program
+(setf prg '(= 1 n)) ; define a very simple program
 (print prg) ; print the program
 ; TODO: execute the program with n = 1 and print its result
+(setf n 1)
+(print(eval prg))
