1c9d0f7523e397ac9ad5048b49bac74c191c7fe3
Dylan <72610316+dylan-mp4@users.noreply.github.com>
2022-01-31 18:32:51 +0000

    task 1.2 a & b

--- lab1-intro/lisp/tasks.lisp
+++ lab1-intro/lisp/tasks.lisp
@@ -1,16 +1,19 @@
 ;; code for Task 1.2(a):
 (print (1 + 2 - 3))
 ;; the above expression throws an error
-
+(print (+ 1 2 (- 3)))
+(terpri)
 ;; code for Task 1.2(b)
 (defvar x 55)
-(cond 
-  ((< x 10) (format t "number below 10"))
-  ((< x 50) (format t "number below 50"))
-  (t (format t "number greater or equal 50"))
-)
+;; (cond 
+;;   ((< x 10) (format t "number below 10"))
+;;   ((< x 50) (format t "number below 50"))
+;;   (t (format t "number greater or equal 50"))
+;; )
 ; TODO: convert the cond macro in a nested if-then-else
-
+(if (< x 10) (format t "number below 10"))
+(if (and (> x 9) (< x 50)) (format t "number below 50"))
+(if (> x 49) (format t "number greater or equal 50"))
 ;; code for Task 1.2(c):
 (setf prg '(+ 1 n)) ; define a very simple program
 (print prg) ; print the program
